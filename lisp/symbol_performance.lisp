a377d4d00434a9e57d80a7c4c50f3858d124980d
Evgeny Khirin <evgeny.khirin@gmail.com>
2020-08-04 11:11:40 +0300

    Added symbols creation performace tests

---
+++ lisp/symbol_performance.lisp
@@ -0,0 +1,56 @@
+(time (dotimes (i 1000000)
+        (symbol-create "abc"))
+      :ops 1000000
+      :name "symbol-create single thread")
+
+(let (threads)
+  (time (progn
+          (dotimes (i os:CPU-ONLINE-COUNT)
+            (push (thread-create (lambda () (dotimes (i 1000000)
+                                              (symbol-create "abc")))
+                                 nil
+                                 :detached nil)
+                  threads))
+          (dolist (thr threads)
+            (thread-join thr)))
+        :ops (* os:CPU-ONLINE-COUNT 1000000)
+        :name "symbol-create in threads"))
+
+(time (dotimes (i 1000000) (gensym))
+      :ops 1000000
+      :name "gensym single thread")
+
+(let (threads)
+  (time (progn
+          (dotimes (i os:CPU-ONLINE-COUNT)
+            (push (thread-create (lambda () (dotimes (i 1000000) (gensym)))
+                                 nil
+                                 :detached nil)
+                  threads))
+          (dolist (thr threads)
+            (thread-join thr)))
+        :ops (* os:CPU-ONLINE-COUNT 1000000)
+        :name "gensym in threads"))
+
+(time (let ((stream (binary-stream-create)))
+        (dotimes (i 10000)
+          (fprintf (binary-stream-rewind stream) "sym-~s" i)
+          (intern (binary-stream-content stream))))
+      :ops 10000
+      :name "intern single thread")
+
+(let (threads)
+  (time (progn
+          (dotimes (i os:CPU-ONLINE-COUNT)
+            (push (thread-create (lambda ()
+                                   (let ((stream (binary-stream-create)))
+                                     (dotimes (i 10000)
+                                       (fprintf (binary-stream-rewind stream) "sym-~s" i)
+                                       (intern (binary-stream-content stream)))))
+                                 nil
+                                 :detached nil)
+                  threads))
+          (dolist (thr threads)
+            (thread-join thr)))
+        :ops (* os:CPU-ONLINE-COUNT 10000)
+        :name "intern in threads"))
